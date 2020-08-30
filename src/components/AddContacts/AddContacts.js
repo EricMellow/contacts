3608340f987b4e366514d1aaacbc056a08015e85
Eric Mellow <ericmellow@gmail.com>
2020-08-30 13:16:06 -0600

    Bind input data to state

--- src/components/AddContacts/AddContacts.js
+++ src/components/AddContacts/AddContacts.js
@@ -11,17 +11,39 @@ class AddContacts extends Component {
     };
   }
 
+  handleChange = (event) => {
+    this.setState({ [event.target.id]: event.target.value });
+  }
+
   render() {
     return (
       <main>
         <form>
           <label for="name">Name:</label>
-          <input type="text" id="name" name="name" required />
+          <input 
+            type="text" 
+            id="name" 
+            name="name" 
+            value={this.state.name} 
+            onChange={this.handleChange}
+            required />
           <label for="email">Email:</label>
-          <input type="email" id="email" required />
+          <input 
+            type="email" 
+            id="email" 
+            value={this.state.email} 
+            onChange={this.handleChange}
+            required />
           <small>Format: you@example.com</small>
           <label for="phone">Phone Number:</label>
-          <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required />
+          <input 
+            type="tel" 
+            id="phone" 
+            name="phone" 
+            pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" 
+            value={this.state.phone} 
+            onChange={this.handleChange}
+            required />
           <small>Format: 123-456-7890</small>
           <input type="submit" value="Save Contact"></input>
         </form>
