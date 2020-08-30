92e7db6cd088758f07153a07db4d7a46cd029e37
Eric Mellow <ericmellow@gmail.com>
2020-08-30 11:58:02 -0600

    Create ContactList component and add to switch route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import './App.css';
 import { NavLink, Route, Switch } from 'react-router-dom';
+import ContactList from '../ContactList/ContactList'
 
 export default function App() {
-
+  
   return (
     <div className="app">
       <header>
         <NavLink to='/'>Home</NavLink>
-        <NavLink to='/Contacts'>Contacts</NavLink>
+        <NavLink to='/Contacts'>Add Contacts</NavLink>
       </header>
       <main>
         <Switch>
           <Route path="/Contacts">
-            <h1>Contacts</h1>
+            <h1>Add Contacts</h1>
           </Route>
           <Route path="/">
-            <h1>Home</h1>
+            <ContactList />
           </Route>
         </Switch>
       </main>
---
+++ src/components/ContactList/ContactList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './ContactList.css';
+
+export default function ContactList() {
+
+  return (
+    <div className="contact-list">
+      <h1>It's the contact list page!</h1>
+    </div>
+  );
+}
