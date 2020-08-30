36b6984cfdb5f9e750ae697c31d5bb7f4323f45b
Eric Mellow <ericmellow@gmail.com>
2020-08-30 13:04:41 -0600

    Add form to AddContacts

--- src/components/AddContacts/AddContacts.js
+++ src/components/AddContacts/AddContacts.js
@@ -5,15 +5,27 @@ class AddContacts extends Component {
   constructor() {
     super();
     this.state = {
-
+      name: '',
+      email: '',
+      phone: '',
     };
   }
 
   render() {
     return (
-      <div>
-        <h1>*ADD CONTACTS HERE*</h1>
-      </div>
+      <main>
+        <form>
+          <label for="name">Name:</label>
+          <input type="text" id="name" name="name" required />
+          <label for="email">Email:</label>
+          <input type="email" id="email" required />
+          <small>Format: you@example.com</small>
+          <label for="phone">Phone Number:</label>
+          <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required />
+          <small>Format: 123-456-7890</small>
+          <input type="submit" value="Save Contact"></input>
+        </form>
+      </main>
     );
   }
 }
